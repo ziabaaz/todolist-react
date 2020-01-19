0d23483d150800888d402f27113849e8f9b0f8d8
Rahman,Zia Ul <ziaul.rahman@honeywell.com>
2020-01-19 17:50:28 +0530

    First Commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                <h1> hello from todo input </h1>
+            </div>
+        )
+    }
+}
